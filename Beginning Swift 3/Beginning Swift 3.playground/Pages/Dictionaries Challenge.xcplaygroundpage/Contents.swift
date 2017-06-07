bacc7ea3a935007f032b29de171abb5ad58ce9bb
Homway <ihomway@live.com>
2017-06-07 23:30:20 +0800

    dicationaries

--- Beginning Swift 3/Beginning Swift 3.playground/Pages/Dictionaries Challenge.xcplaygroundpage/Contents.swift
+++ Beginning Swift 3/Beginning Swift 3.playground/Pages/Dictionaries Challenge.xcplaygroundpage/Contents.swift
@@ -1,7 +1,37 @@
 //: [Previous](@previous)
 
-import Foundation
+import UIKit
 
-var str = "Hello, playground"
+/*:
+#### Beginnng Swift Video Tutorial Series - raywenderlich.com
+#### Video 12: Dictionaries
 
+**Note:** If you're seeing this text as comments rather than nicely-rendered text, select Editor\Show Rendered Markup in the Xcode menu.
+*/
+
+//: Create a new dictionary and add the following values: Stephen King - Under the Dome, Elizabeth Peters - Crocodile on the Sandbank, Clive Cussler - The Wrecker
+var authorInfo = ["Stephen King": "Under the Dome", "Elizabeth Peters": "Crocodile on the Sandbank", "Clive Cussler": "The Wrecker"]
+//: Add a new key: Robert Heinlein - The Moon is a Harsh Mistress
+authorInfo["Robert Heinlein"] = "The Moon is a Harsh Mistress"
+//: Print out the Stephen King value
+print(authorInfo["Stephen King"])
+//: Now delete the Stephen King key/value pair
+
+//: Loop through the dictionary and print out all the key/values
+for (key, value) in authorInfo {
+	print("\(key) - \(value)")
+}
+//: **Ub3r H4ck3r Challenge** Declare a function occurrencesOfCharacters that calculates which characters occur in a string, as well as how often each of these characters occur. Return the result as a dictionary: func occurrencesOfCharacters(text: String) -> [Character: Int]
+func occurrencesOfCharacters(text: String) -> [Character: Int] {
+	var chars = [Character: Int]()
+	
+	for character in text.characters {
+		let count = chars[character] ?? 0
+		chars[character] = count + 1
+	}
+	
+	return chars
+}
+
+print(occurrencesOfCharacters(text: "These are the days of our lines"))
 //: [Next](@next)
--- Beginning Swift 3/Beginning Swift 3.playground/Pages/Dictionaries.xcplaygroundpage/Contents.swift
+++ Beginning Swift 3/Beginning Swift 3.playground/Pages/Dictionaries.xcplaygroundpage/Contents.swift
@@ -4,4 +4,30 @@ import Foundation
 
 var str = "Hello, playground"
 
-//: [Next](@next)
+var userInfo = ["firstName": "Chris", "lastName": "Belanger", "position" : "Editor"]
+print(userInfo["firstName"] as Any)
+
+if let firstName = userInfo["firstName"], let lastName = userInfo["lastName"] {
+	print("\(firstName) \(lastName)")
+}
+
+var anotherUser: [String: String] = [:]
+anotherUser["firstName"] = "Luke"
+anotherUser["lastName"] = "Freeman"
+
+if anotherUser["firstName"] != nil && anotherUser["lastName"] != nil {
+	print("\(anotherUser["firstName"]!) \(anotherUser["lastName"]!)")
+}
+
+anotherUser["lastName"] = nil
+print(anotherUser)
+
+for key in anotherUser.keys {
+	if let info = anotherUser[key] {
+		print(info)
+	}
+}
+
+for (_, value) in anotherUser {
+	print(value)
+}
