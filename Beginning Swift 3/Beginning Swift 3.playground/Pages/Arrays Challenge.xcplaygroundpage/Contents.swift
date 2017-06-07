31d17e82201242076fed93fe4a947ac832cd255b
Homway <ihomway@live.com>
2017-06-07 23:12:53 +0800

    Arrays

--- Beginning Swift 3/Beginning Swift 3.playground/Pages/Arrays Challenge.xcplaygroundpage/Contents.swift
+++ Beginning Swift 3/Beginning Swift 3.playground/Pages/Arrays Challenge.xcplaygroundpage/Contents.swift
@@ -1,7 +1,32 @@
 //: [Previous](@previous)
 
-import Foundation
+import UIKit
 
-var str = "Hello, playground"
+/*:
+#### Beginnng Swift Video Tutorial Series - raywenderlich.com
+#### Video 11: Arrays
 
+**Note:** If you're seeing this text as comments rather than nicely-rendered text, select Editor\Show Rendered Markup in the Xcode menu.
+*/
+
+//: Create an array called numbers with the following numbers: 54 10 88 43
+let numbers = [54, 10, 88, 43]
+//: Create an empty array, called moreNumbers. Next, append those numbers to it.
+var moreNumbers: [Int] = []
+moreNumbers.append(contentsOf: numbers)
+//: Remove the numbers 54 and 43.
+moreNumbers.remove(at: 0)
+moreNumbers.remove(at: 2)
+//: **Ub3r H4ck3r Challenge** Arrays have a reversed() method that returns an array holding the same elements as the original array, in reverse order. Write a function that does the same thing, without using reversed(). This is the signature of the function:  func reversed(_ myArray: [Int]) -> [Int]
+func reversed(_ myArray:[Int]) -> [Int] {
+	var reversedArray = [Int]()
+	
+	for number in myArray {
+		reversedArray.insert(number, at: 0)
+	}
+	
+	return reversedArray
+}
+
+reversed(moreNumbers)
 //: [Next](@next)
--- Beginning Swift 3/Beginning Swift 3.playground/Pages/Arrays.xcplaygroundpage/Contents.swift
+++ Beginning Swift 3/Beginning Swift 3.playground/Pages/Arrays.xcplaygroundpage/Contents.swift
@@ -4,4 +4,32 @@ import Foundation
 
 var str = "Hello, playground"
 
+var books = ["The Stand", "Game of Thrones", "Levithan Wakes", "The Lost World"]
+print(books)
+print(books[0])
+books[0] = "The Gunslinger"
+print(books[0])
+//books[1] = 10
+
+books.append("Red Rising")
+print(books)
+books.remove(at: 4)
+print(books)
+books += ["Red Rising", "Golden Son"]
+print(books)
+
+var authors: [String] = []
+//authors[0] = "Stephen King"
+authors.append("Stephen King")
+authors[0] = "George R R Martin"
+print(authors)
+
+for index in 0..<books.count {
+	print(books[index])
+}
+
+for book in books {
+	print(book)
+}
+
 //: [Next](@next)
