c7fc9fb7d6e571cc3b35045e216cd6b7b57ea1f1
Homway <ihomway@live.com>
2017-06-07 22:54:14 +0800

    optionals

--- Beginning Swift 3/Beginning Swift 3.playground/Pages/Optionals Challenge.xcplaygroundpage/Contents.swift
+++ Beginning Swift 3/Beginning Swift 3.playground/Pages/Optionals Challenge.xcplaygroundpage/Contents.swift
@@ -1,7 +1,37 @@
 //: [Previous](@previous)
 
-import Foundation
+import UIKit
 
-var str = "Hello, playground"
+/*:
+#### Beginnng Swift Video Tutorial Series - raywenderlich.com
+#### Video 10: Optionals
 
+**Note:** If you're seeing this text as comments rather than nicely-rendered text, select Editor\Show Rendered Markup in the Xcode menu.
+*/
+
+//: Make an optional String called myFavoriteSong. If you have a favorite song, set it to a string representing that song. If you have more than one favorite song or no favorite, set the optional to nil.
+let myFavoriteSong: String? = nil
+
+//: Create two variables. Call one name and the other is called greeting. The greeting should be "Hello,". Unwrap the name variable, and if there's a name value, then print out the greeting
+let name: String? = "Homway"
+let greeting: String = "Hello"
+
+if let name = name {
+	print("\(greeting) \(name)")
+}
+
+//: Now, write the greeting using force unwrap.
+print("\(greeting) \(name!)")
+
+//: **Ub3r H4ck3r Challenge** - Create a function that prints out the greeting. This function will take two strings - both optionals. Use guard syntax to bind the variable and print out the greeting.
+
+func greeting(_ greeting: String?, to name: String?) {
+	guard let greeting = greeting, let name = name else {
+		return;
+	}
+	
+	print("\(greeting) \(name)")
+}
+
+greeting("Hey", to: "Juice")
 //: [Next](@next)
--- Beginning Swift 3/Beginning Swift 3.playground/Pages/Optionals.xcplaygroundpage/Contents.swift
+++ Beginning Swift 3/Beginning Swift 3.playground/Pages/Optionals.xcplaygroundpage/Contents.swift
@@ -4,4 +4,39 @@ import Foundation
 
 var str = "Hello, playground"
 
+var firstName: String? = "Nic"
+var lastName: String? = "Pringle"
+
+print(firstName as Any)
+
+if let firstName = firstName {
+	print(firstName)
+	if let lastName = lastName {
+		print(lastName)
+	}
+}
+
+if let firstName = firstName, let lastName = lastName {
+	print("\(firstName) \(lastName)")
+}
+
+if var firstName = firstName, var lastName = lastName {
+	firstName = "Sam"
+	lastName = "Davies"
+	print("\(firstName) \(lastName)")
+}
+
+func print(firstName: String?, andLastName lastName:String?) {
+	guard let firstName = firstName, let lastName = lastName else {
+		return
+	}
+	
+	print("\(firstName) \(lastName)")
+}
+
+print(firstName: "Ray", andLastName: "Wenderlich")
+print(firstName: nil, andLastName: "Wenderlich")
+
+//func print()
+
 //: [Next](@next)
