482a9f09a86234b1a690c6849dbe990ccd77acca
Homway <ihomway@live.com>
2017-06-10 15:54:46 +0800

    Properties

--- Intermediate Swift 3/Intermediate Swift 3.playground/Pages/Properties.xcplaygroundpage/Contents.swift
+++ Intermediate Swift 3/Intermediate Swift 3.playground/Pages/Properties.xcplaygroundpage/Contents.swift
@@ -4,4 +4,46 @@ import Foundation
 
 var str = "Hello, playground"
 
+struct Book {
+	var title: String
+	var isPublished: Bool
+}
+
+struct Author {
+	var firstName: String
+	var lastName: String
+	var booksWritten: [Book] = []
+	var booksBeingWritten: [Book] = []
+	var books: [Book] {
+		get {
+			return booksWritten
+		}
+	}
+	
+	var totalBooks: Int {
+		return booksBeingWritten.count + booksWritten.count
+	}
+	
+	mutating func addBook(_ aBook: Book) {
+		if aBook.isPublished {
+			booksWritten.append(aBook)
+		} else {
+			booksBeingWritten.append(aBook)
+		}
+	}
+}
+
+var theDarkTower = Book(title: "The Dark Tower", isPublished: true)
+var theStand = Book(title: "The Stand", isPublished: true)
+
+var writer = Author(firstName: "Stephen", lastName: "King", booksWritten: [theStand], booksBeingWritten: [])
+var anotherBook = Book(title: "The Gungslinger", isPublished: true)
+writer.addBook(anotherBook)
+
+var unPublishedBook = Book(title: "Untitled Project", isPublished: false)
+writer.addBook(unPublishedBook)
+writer.booksBeingWritten
+writer.booksWritten
+writer.totalBooks
+
 //: [Next](@next)
