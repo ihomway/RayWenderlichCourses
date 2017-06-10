0ea6c8d26170c284cc182e35c6b081c84eea7403
Homway <ihomway@live.com>
2017-06-10 15:28:24 +0800

    Structure

--- Intermediate Swift 3/Intermediate Swift 3.playground/Pages/Stuctures Challenge.xcplaygroundpage/Contents.swift
+++ Intermediate Swift 3/Intermediate Swift 3.playground/Pages/Stuctures Challenge.xcplaygroundpage/Contents.swift
@@ -1,7 +1,58 @@
 //: [Previous](@previous)
 
-import Foundation
+import UIKit
 
-var str = "Hello, playground"
+/*:
+#### Intermediate Swift Video Tutorial Series - raywenderlich.com
+#### Video 3: Structures
+
+**Note:** If you're seeing this text as comments rather than nicely-rendered text, select Editor\Show Rendered Markup in the Xcode menu.
+*/
+
+//: Create a T-shirt struct that has size, color and material options. The size prices go from 3, 5, 7. The colors range from red, blue, and white. It should range from 2, 3, and 1. Finally, for material options, choose regular or organic. It should be 5 or 10. Print out the result in calculatePrice()
+
+struct Size {
+	var name: String
+	var price: Int
+}
+
+struct Color {
+	var name: String
+	var price: Int
+}
+
+struct Material {
+	var name: String
+	var price: Int
+}
+
+struct TShirt {
+	var size: Size
+	var color: Color
+	var material: Material
+	
+	func calculatePrice() -> Int {
+		var price = 0
+		price += size.price
+		price += color.price
+		price += material.price
+		
+		return price
+	}
+}
+
+//: Here's an example of creating a struct:
+//: var tshirt = TShirt(size: "M", color: "red", material: "organic")
+
+//var tshirt = TShirt(size: "M", color: "red", material: "organic")
+
+
+//: **Ub3r H4ck3r Challenge** Refactor the code so that materials, color, and sizes are structs with a name and a price. Pass these structs into the TShirt and have it calculate the price
+
+
+var tshirt = TShirt(size: Size(name:"M", price: 3), color: Color(name:"red", price: 2), material: Material(name:"organic", price: 10))
+tshirt.calculatePrice()
+
+
 
 //: [Next](@next)
--- Intermediate Swift 3/Intermediate Swift 3.playground/Pages/Stuctures.xcplaygroundpage/Contents.swift
+++ Intermediate Swift 3/Intermediate Swift 3.playground/Pages/Stuctures.xcplaygroundpage/Contents.swift
@@ -4,4 +4,27 @@ import Foundation
 
 var str = "Hello, playground"
 
+struct Book {
+	var title: String
+	
+}
+
+struct Author {
+	var firstName: String
+	var lastName: String
+	var books: [Book] = []
+	
+	mutating func addBook(_ aBook: Book) {
+		books.append(aBook)
+	}
+}
+
+var book = Book(title: "The Stand")
+book.title
+
+var write = Author(firstName: "Stephen", lastName: "King", books: [book])
+var anotherBook = Book(title: "The Gunslinger")
+
+write.addBook(anotherBook)
+
 //: [Next](@next)
