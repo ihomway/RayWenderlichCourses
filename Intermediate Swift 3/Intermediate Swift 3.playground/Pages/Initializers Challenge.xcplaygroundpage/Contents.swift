d8c6831de9926e22e3f3c2f6417e00d7705a3436
Homway <ihomway@live.com>
2017-06-11 22:02:25 +0800

    Initializers

--- Intermediate Swift 3/Intermediate Swift 3.playground/Pages/Initializers Challenge.xcplaygroundpage/Contents.swift
+++ Intermediate Swift 3/Intermediate Swift 3.playground/Pages/Initializers Challenge.xcplaygroundpage/Contents.swift
@@ -1,7 +1,42 @@
 //: [Previous](@previous)
 
-import Foundation
+import UIKit
 
-var str = "Hello, playground"
+/*:
+#### Intermediate Swift Video Tutorial Series - raywenderlich.com
+#### Video 7: Initializers
+
+**Note:** If you're seeing this text as comments rather than nicely-rendered text, select Editor\Show Rendered Markup in the Xcode menu.
+
+*/
+
+//: Create a story class that contains two string properties: name and a writer. Provide an a init() that sets both properties
+class Story {
+	var name: String
+	var writer: String
+	
+	init(name: String, writer: String) {
+		self.name = name
+		self.writer = writer
+	}
+}
+
+//: Create a new Movie class that is a sublcass of the story class. Give it a new string propery called directory. Create a init that sets the director, name, and writer properties.
+
+class Movie: Story {
+	var directory: String
+	
+	init(name: String, writer: String, directory: String) {
+		self.directory = directory
+		super.init(name: name, writer: writer)
+	}
+	
+	convenience init(directory: String) {
+		self.init(name: "Unknown Title", writer: "Unknown writer", directory: directory)
+	}
+}
+
+//: Create a convenience init that takes just a director's name. Set the name to "Unknown Title" and the writer to "Unknown writer"
+
 
 //: [Next](@next)
--- Intermediate Swift 3/Intermediate Swift 3.playground/Pages/Initializers.xcplaygroundpage/Contents.swift
+++ Intermediate Swift 3/Intermediate Swift 3.playground/Pages/Initializers.xcplaygroundpage/Contents.swift
@@ -4,4 +4,55 @@ import Foundation
 
 var str = "Hello, playground"
 
+struct Page {
+	var words = 0
+}
+
+class Story {
+	var title: String
+	var author: String
+	var year: Int
+	var pages: [Page]
+	
+	init(title: String, author: String, year: Int) {
+		self.title = title;
+		self.author = author
+		self.year = year
+		pages = [Page]()
+	}
+	
+	convenience init(title: String) {
+		self.init(title: title, author: "Unkown", year: 2016)
+	}
+	
+	func addPage(_ page: Page) {
+		pages.append(page)
+	}
+}
+
+class ShortStory: Story {
+	var maxPageCount = 2
+	
+	init() {
+		maxPageCount = 2
+		
+		super.init(title: "Unkown Story", author: "Unknown Author", year: 2016)
+	}
+	
+	override func addPage(_ page: Page) {
+		if pages.count < maxPageCount {
+			super.addPage(page)
+		}
+	}
+}
+
+var story = ShortStory()
+story.title = "The Haunting of Hill house"
+story.addPage(Page(words: 300))
+story.addPage(Page(words: 300))
+story.addPage(Page(words: 300))
+story.addPage(Page(words: 300))
+story.pages.count
+story.author
+
 //: [Next](@next)
