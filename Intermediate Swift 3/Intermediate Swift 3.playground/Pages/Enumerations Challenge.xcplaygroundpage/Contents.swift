5112342db9ab11a31d46636605b9bfb0b5eb701d
Homway <ihomway@live.com>
2017-06-11 22:38:03 +0800

    Enumerations

--- Intermediate Swift 3/Intermediate Swift 3.playground/Pages/Enumerations Challenge.xcplaygroundpage/Contents.swift
+++ Intermediate Swift 3/Intermediate Swift 3.playground/Pages/Enumerations Challenge.xcplaygroundpage/Contents.swift
@@ -1,7 +1,39 @@
 //: [Previous](@previous)
 
-import Foundation
+import UIKit
 
-var str = "Hello, playground"
+/*:
+#### Intermediate Swift Video Tutorial Series - raywenderlich.com
+#### Video 9: Enumerations
+
+**Note:** If you're seeing this text as comments rather than nicely-rendered text, select Editor\Show Rendered Markup in the Xcode menu.
+**/
+
+//: Create a stars rating system that have the following cases: 1 star, 2 stars, 3 stars, 4 stars, 5 stars. Give each case the following values: terrible, meh, good, great, classic
+enum Rating: String {
+case oneStars = "terrible", twoStars = "meh", threeStars = "good", fourStars = "greet", fiveStars = "classic"
+}
+
+//: Next, add the following, "The Wizard of Oz is rated 5 stars. This makes it (five stars value)
+
+print("This Wizard of Oz is rated 4 stars. This makes it \(Rating.fourStars.rawValue)")
+
+//: Create an enumeration called Months that will hold all the cases for months in the year. Make this enumeration an Int type. Start counting from 1.
+
+enum Month: Int {
+	case january = 1, february, march, april, may, june, july, august, september, october, november, december
+}
+
+//: //: **Ub3r H4ck3r Challenge** Create a new enumeration function that determines the amount of time for the winter break. The winter break occurs in December. The function should take a month and return an Int. The signature should be something like: func monthsUntilWinterBreak(from month: Month) -> Int
+//: You may want to create a new enumeration for this function
+
+extension Month {
+	func monthsUntilWinterBreak(from month: Month) -> Int {
+		return Month.december.rawValue - month.rawValue
+	}
+}
+
+Month.september.monthsUntilWinterBreak(from: Month.august)
+
 
 //: [Next](@next)
--- Intermediate Swift 3/Intermediate Swift 3.playground/Pages/Enumerations.xcplaygroundpage/Contents.swift
+++ Intermediate Swift 3/Intermediate Swift 3.playground/Pages/Enumerations.xcplaygroundpage/Contents.swift
@@ -4,4 +4,73 @@ import Foundation
 
 var str = "Hello, playground"
 
+enum Coin: Int {
+	case penny = 1
+	case nickel = 5
+	case dime = 10
+	case quarter = 25
+}
+
+var coin = Coin.quarter
+
+switch coin {
+case .penny:
+	print("you have a penny")
+case .nickel:
+	print("you have a nickel")
+case .dime:
+	print("you habve a dime")
+case .quarter:
+	print("you have a quarter")
+}
+
+var coins: [Coin] = [.quarter, .quarter, .nickel, .dime, .quarter]
+
+var value = 0
+for coin in coins {
+	
+	switch coin {
+	case .penny:
+		print("you have a penny")
+	case .nickel:
+		print("you have a nickel")
+	case .dime:
+		print("you habve a dime")
+	case .quarter:
+		print("you have a quarter")
+	}
+	
+	value += coin.rawValue
+}
+
+enum Icon: String {
+	case music
+	case sports
+	case weather
+	
+	var fileName: String {
+		return "\(rawValue.capitalized).png"
+	}
+}
+
+let icon = Icon.weather
+icon.fileName
+
+enum HTTPMethod {
+	case get(address: String)
+	case post(body: String)
+}
+
+func makeRequest(method: HTTPMethod) {
+	switch method {
+	case .get(let address):
+		print("Address: \(address)")
+	case .post(let body):
+		print("Body: \(body)")
+	}
+}
+
+makeRequest(method: .get(address: "www.raywenderlich.com"))
+makeRequest(method: .post(body: "Hello World"))
+
 //: [Next](@next)
