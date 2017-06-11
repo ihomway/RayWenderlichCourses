83d33143da83707ffe68d80b140793009fe7c123
Homway <ihomway@live.com>
2017-06-11 21:47:26 +0800

    Inheritance

--- Intermediate Swift 3/Intermediate Swift 3.playground/Pages/Inheritance Challenge.xcplaygroundpage/Contents.swift
+++ Intermediate Swift 3/Intermediate Swift 3.playground/Pages/Inheritance Challenge.xcplaygroundpage/Contents.swift
@@ -1,7 +1,36 @@
 //: [Previous](@previous)
 
-import Foundation
+import UIKit
 
-var str = "Hello, playground"
+/*:
+#### Intermediate Swift Video Tutorial Series - raywenderlich.com
+#### Video 6: Initializers
+
+**Note:** If you're seeing this text as comments rather than nicely-rendered text, select Editor\Show Rendered Markup in the Xcode menu.
+
+*/
+
+//: Create a class called Account that has a balance property. Have the balance set to 0. Create a function to withdrawl money from the balance. Make sure the withdrawl amount is greater than 0 and the amount is less than the balance.
+
+class Account {
+	var balance: Double = 0.0
+	
+	func withdrawl(_ amount: Double) {
+		if amount > 0 && amount < balance {
+			balance -= amount
+		}
+	}
+}
+
+//: Create a subclass called SavingsAccount and override withdrawl(). In this method, make sure that the withdrawn amount is greater than 10.
+
+class SavingsAccount: Account {
+	override func withdrawl(_ amount: Double) {
+		if amount > 10 {
+			super.withdrawl(amount)
+		}
+	}
+}
+
 
 //: [Next](@next)
--- Intermediate Swift 3/Intermediate Swift 3.playground/Pages/Inheritance.xcplaygroundpage/Contents.swift
+++ Intermediate Swift 3/Intermediate Swift 3.playground/Pages/Inheritance.xcplaygroundpage/Contents.swift
@@ -4,4 +4,36 @@ import Foundation
 
 var str = "Hello, playground"
 
+struct Page {
+	var words = 0
+}
+
+class Story {
+	var title = ""
+	var pages: [Page] = []
+	
+	func addPage(_ page: Page) {
+		pages.append(page)
+	}
+}
+
+class ShortStory: Story {
+	var maxPageCount = 2
+	
+	override func addPage(_ page: Page) {
+		if pages.count < maxPageCount {
+			super.addPage(page)
+		}
+	}
+}
+
+var story = ShortStory()
+story.title = "The Haunting of Hill house"
+story.addPage(Page(words: 300))
+story.addPage(Page(words: 300))
+story.addPage(Page(words: 300))
+story.addPage(Page(words: 300))
+story.pages.count
+
+
 //: [Next](@next)
