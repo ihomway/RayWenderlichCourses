f352b401148b5609f225442d6a313d687e77e571
Homway <ihomway@live.com>
2017-06-11 21:34:50 +0800

    Classes

--- Intermediate Swift 3/Intermediate Swift 3.playground/Pages/Classes Challenge.xcplaygroundpage/Contents.swift
+++ Intermediate Swift 3/Intermediate Swift 3.playground/Pages/Classes Challenge.xcplaygroundpage/Contents.swift
@@ -1,9 +1,42 @@
 //: [Previous](@previous)
 
-import Foundation
+import UIKit
 
-var str = "Hello, playground"
+/*:
+#### Intermediate Swift Video Tutorial Series - raywenderlich.com
+#### Video 5: Classes
 
+**Note:** If you're seeing this text as comments rather than nicely-rendered text, select Editor\Show Rendered Markup in the Xcode menu.
 
+Make the following objects and determine whether they are structs or classes. For the properties, use properties unless noted below.
+
+TShirt: size, color
+Address: street, city, state, zipCode
+User: firstName, lastName, address (Address object)
+ShoppingCart: shirts (array of TShirt), User (user object)
+
+*/
+
+struct Tshirt {
+	
+}
+
+struct Address {
+	var street: String = ""
+	var city: String = ""
+	var state: String = ""
+	var zipCode: Int = 0
+}
+
+class User {
+	var firstName = ""
+	var lastName = ""
+	var address: Address = Address()
+}
+
+class ShoppingCart {
+	var shirts: [Tshirt] = []
+	var user = User()
+}
 
 //: [Next](@next)
--- Intermediate Swift 3/Intermediate Swift 3.playground/Pages/Classes.xcplaygroundpage/Contents.swift
+++ Intermediate Swift 3/Intermediate Swift 3.playground/Pages/Classes.xcplaygroundpage/Contents.swift
@@ -4,4 +4,23 @@ import Foundation
 
 var str = "Hello, playground"
 
+class Movie {
+	var title = ""
+	var runningTime = 0
+}
+
+var adventureMovie = Movie()
+adventureMovie.title = "Raiders of the Lost Ark"
+adventureMovie.runningTime = 90
+var scienceFictionMovie = adventureMovie
+
+scienceFictionMovie.title = "2001"
+print(adventureMovie.title)
+print(scienceFictionMovie.title)
+print(adventureMovie === scienceFictionMovie)
+
+scienceFictionMovie = Movie()
+scienceFictionMovie.title = "The Martian"
+print(adventureMovie === scienceFictionMovie)
+
 //: [Next](@next)
