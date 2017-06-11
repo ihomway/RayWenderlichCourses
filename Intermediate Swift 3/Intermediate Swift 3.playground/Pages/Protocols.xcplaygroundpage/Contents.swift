af40c237f88d097d46300f725f4bd826000c39c8
Homway <ihomway@live.com>
2017-06-11 22:17:25 +0800

    Protocols

--- Intermediate Swift 3/Intermediate Swift 3.playground/Pages/Protocols.xcplaygroundpage/Contents.swift
+++ Intermediate Swift 3/Intermediate Swift 3.playground/Pages/Protocols.xcplaygroundpage/Contents.swift
@@ -4,4 +4,76 @@ import Foundation
 
 var str = "Hello, playground"
 
+protocol Vehicle {
+	func accelerate()
+	func stop()
+}
+
+protocol Music {
+	func play()
+}
+
+class Transport {
+	
+}
+
+class Unicycle: Vehicle {
+	var peddling = false
+	
+	func accelerate() {
+		print("squeaky squeaky")
+		peddling = true
+	}
+	
+	func stop() {
+		peddling = false
+	}
+}
+
+class Car: Transport, Vehicle, Music {
+	var velocity = 0
+	func accelerate() {
+		print("vrrrroom")
+		velocity += 4
+	}
+	
+	func stop() {
+		velocity = 0
+	}
+	
+	func play() {
+		//
+	}
+}
+
+class Cat {
+	
+}
+
+class Boat {
+	var knot = 0
+}
+
+extension Boat: Vehicle {
+	func accelerate() {
+		print("swish swish")
+		knot += 10
+	}
+	
+	func stop() {
+		knot = 0
+	}
+}
+
+var vehicles: [Vehicle] = []
+vehicles.append(Car())
+vehicles.append(Unicycle())
+//vehicles.append(Cat())
+vehicles.append(Boat())
+
+for vehicle in vehicles {
+	vehicle.accelerate()
+	//	vehicle.stop()
+}
+
 //: [Next](@next)
