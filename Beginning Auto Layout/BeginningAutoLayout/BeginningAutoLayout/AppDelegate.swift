246dc7fcf42c5d2812b36072774788889f7e3e95
ihomway <ihomway@live.com>
2017-06-26 15:46:17 +0800

    Autoresizing Masks

---
+++ Beginning Auto Layout/BeginningAutoLayout/BeginningAutoLayout/AppDelegate.swift
@@ -0,0 +1,23 @@
+//
+//  AppDelegate.swift
+//  BeginningAutoLayout
+//
+//  Created by HOMWAY on 26/06/2017.
+//  Copyright © 2017 ihomway. All rights reserved.
+//
+
+import UIKit
+
+@UIApplicationMain
+class AppDelegate: UIResponder, UIApplicationDelegate {
+
+	var window: UIWindow?
+
+
+	func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplicationLaunchOptionsKey: Any]?) -> Bool {
+		// Override point for customization after application launch.
+		return true
+	}
+
+}
+
