cf2a04b8a423e3b6fd573891353e5d8c876ede67
Homway <ihomway@live.com>
2017-06-29 00:20:16 +0800

    Adaptive Layout Enviroment Challenge

--- Mastering Auto Layout/MasteringAutoLayout/MasteringAutoLayout/ViewController.swift
+++ Mastering Auto Layout/MasteringAutoLayout/MasteringAutoLayout/ViewController.swift
@@ -10,16 +10,70 @@ import UIKit
 
 class ViewController: UIViewController {
 
+	 @IBOutlet var buttonStackView: UIStackView!
+	var weatherViewController: WeatherViewController?
+
 	override func viewDidLoad() {
 		super.viewDidLoad()
 		// Do any additional setup after loading the view, typically from a nib.
+
+		if traitCollection.horizontalSizeClass == .regular {
+			setupWeatherView()
+			weatherViewController?.view.isHidden = false
+		}
 	}
 
-	override func didReceiveMemoryWarning() {
-		super.didReceiveMemoryWarning()
-		// Dispose of any resources that can be recreated.
+	override func willTransition(to newCollection: UITraitCollection, with coordinator: UIViewControllerTransitionCoordinator) {
+		super.willTransition(to: newCollection, with: coordinator)
+
+		if newCollection.horizontalSizeClass == .regular, newCollection.horizontalSizeClass != traitCollection.horizontalSizeClass {
+			setupWeatherView()
+		} else if newCollection.horizontalSizeClass == .compact {
+			removeWeatherView()
+		}
+
+		coordinator.animate(alongsideTransition: nil) { (context) in
+			self.weatherViewController?.view.isHidden = false
+		}
 	}
 
+	private func setupWeatherView() {
+		guard let storyboard = storyboard else {
+			return
+		}
+		guard let weatherViewController = storyboard.instantiateViewController(withIdentifier: String(describing: WeatherViewController.self)) as? WeatherViewController else {
+			return
+		}
+
+		weatherViewController.view.translatesAutoresizingMaskIntoConstraints = false
+		weatherViewController.willMove(toParentViewController: self)
+		weatherViewController.view.isHidden = true
+
+		weatherViewController.backgroundImageView.isHidden = true
+		view.addSubview(weatherViewController.view)
+
+		weatherViewController.view.centerYAnchor.constraint(equalTo: buttonStackView.centerYAnchor).isActive = true
+		weatherViewController.view.leadingAnchor.constraint(equalTo: buttonStackView.trailingAnchor, constant: 100).isActive = true
+		addChildViewController(weatherViewController)
+
+		let horizontallyCompact = UITraitCollection(horizontalSizeClass: .compact)
+		let verticallyCompact = UITraitCollection(verticalSizeClass: .compact)
+		let compact = UITraitCollection(traitsFrom: [horizontallyCompact, verticallyCompact])
+		setOverrideTraitCollection(compact, forChildViewController: weatherViewController)
+
+		self.weatherViewController = weatherViewController
+	}
+
+	private func removeWeatherView() {
+		guard let weatherViewController = weatherViewController else {
+			return
+		}
+
+		weatherViewController.willMove(toParentViewController: nil)
+		weatherViewController.view.removeFromSuperview()
+		weatherViewController.removeFromParentViewController()
+		self.weatherViewController = nil
+	}
 
 }
 
--- Mastering Auto Layout/MasteringAutoLayout/MasteringAutoLayout/WeatherViewController.swift
+++ Mastering Auto Layout/MasteringAutoLayout/MasteringAutoLayout/WeatherViewController.swift
@@ -9,65 +9,33 @@
 import UIKit
 
 class WeatherViewController: UIViewController {
-	let daysToForecast = 3
-	let gap: CGFloat = 8.0
-	var imageViews: [UIImageView] = []
-	var imageConstraints: [NSLayoutConstraint] = []
-	let leftGapGuide = UILayoutGuide()
-	let rightGapGuide = UILayoutGuide()
-	
-	fileprivate func setupImageConstraints(forSizeClass sizeClass: UIUserInterfaceSizeClass) {
-		NSLayoutConstraint.deactivate(imageConstraints)
-		imageConstraints.removeAll()
-
-		let imageWidth = imageViews[0].image!.imageAsset!.image(with: UITraitCollection(horizontalSizeClass: sizeClass)).size.width
-		let views: [String: Any] = ["monday": imageViews[0], "tuesday": imageViews[1], "wednesday": imageViews[2], "leftGap": leftGapGuide, "rightGap": rightGapGuide]
-		let metrics = ["fullWidth": imageWidth, "smaller": imageWidth * 0.7, "bigGap": gap * 3, "gap": gap]
-		imageConstraints.append(contentsOf: NSLayoutConstraint.constraints(withVisualFormat: "H:|[leftGap]-[monday(fullWidth@849)]-(bigGap)-[tuesday(smaller@849)]-(gap)-[wednesday(smaller@849)]-(gap)-[rightGap(==leftGap)]|", options: .alignAllCenterY, metrics: metrics, views: views))
-		imageConstraints.append(imageViews[0].centerYAnchor.constraint(equalTo: view.centerYAnchor))
-
-
-		NSLayoutConstraint.activate(imageConstraints)
-	}
+	@IBOutlet weak var backgroundImageView: UIImageView!
+	@IBOutlet weak var stackView: UIStackView!
 
 	override func viewDidLoad() {
 		super.viewDidLoad()
 		
 		// Do any additional setup after loading the view.
-		
-		for day in 0..<daysToForecast {
-			let weatherImage: UIImageView
-			switch day {
-			case 0:
-				weatherImage = UIImageView(image: #imageLiteral(resourceName: "weather-sun"))
-			case 1:
-				weatherImage = UIImageView(image: #imageLiteral(resourceName: "weather-windy"))
-			case 2:
-				weatherImage = UIImageView(image: #imageLiteral(resourceName: "weather-thunderstorm"))
-			default:
-				fatalError()
-			}
-			
-			weatherImage.translatesAutoresizingMaskIntoConstraints = false
-			view.addSubview(weatherImage)
-			imageViews.append(weatherImage)
-		}
-		
-		for imageView in imageViews {
-			imageView.widthAnchor.constraint(equalTo: imageView.heightAnchor).isActive = true
-		}
-
-		view.addLayoutGuide(leftGapGuide)
-		view.addLayoutGuide(rightGapGuide)
-
-		setupImageConstraints(forSizeClass: self.traitCollection.horizontalSizeClass)
+		setupImages(forTraitCollection: traitCollection)
 	}
 
 	override func willTransition(to newCollection: UITraitCollection, with coordinator: UIViewControllerTransitionCoordinator) {
 		super.willTransition(to: newCollection, with: coordinator)
 
-		if newCollection.horizontalSizeClass != traitCollection.horizontalSizeClass {
-			setupImageConstraints(forSizeClass: newCollection.horizontalSizeClass)
+		if newCollection.horizontalSizeClass != traitCollection.horizontalSizeClass ||
+			newCollection.verticalSizeClass != traitCollection.verticalSizeClass {
+			setupImages(forTraitCollection: newCollection)
+		}
+	}
+
+	private func setupImages(forTraitCollection traintCollection: UITraitCollection) {
+		guard stackView.arrangedSubviews.count > 1 else {
+			return
+		}
+
+		let compact = traintCollection.horizontalSizeClass == .compact && traintCollection.verticalSizeClass == .compact
+		for subview in stackView.arrangedSubviews.suffix(from: 1) {
+			subview.isHidden = compact
 		}
 	}
 }
