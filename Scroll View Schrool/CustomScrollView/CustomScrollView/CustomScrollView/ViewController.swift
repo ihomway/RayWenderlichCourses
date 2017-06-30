aee331c4a52d2966005e1414a9d256d3b8f66888
ihomway <ihomway@live.com>
2017-06-30 17:41:02 +0800

    zooming

--- Scroll View Schrool/CustomScrollView/CustomScrollView/CustomScrollView/ViewController.swift
+++ Scroll View Schrool/CustomScrollView/CustomScrollView/CustomScrollView/ViewController.swift
@@ -17,7 +17,20 @@ class ViewController: UIViewController {
 		super.viewDidLoad()
 		// Do any additional setup after loading the view, typically from a nib.
 		
-		scrollView.contentSize = imageView.image!.size
+//		scrollView.contentSize = imageView.image!.size
+		imageView.frame.size = (imageView.image?.size)!
+		setZoomParametersForSize(scrollView.frame.size)
+	}
+	
+	func setZoomParametersForSize(_ scrollViewSize: CGSize) {
+		let imageSize = imageView.bounds.size
+		let widthScale = scrollViewSize.width / imageSize.width
+		let heightScale = scrollViewSize.height / imageSize.height
+		let minScale = min(widthScale, heightScale)
+		
+		scrollView.minimumZoomScale = minScale
+		scrollView.maximumZoomScale = 3.0
+		scrollView.zoomScale = minScale
 	}
 
 	override func didReceiveMemoryWarning() {
@@ -25,6 +38,14 @@ class ViewController: UIViewController {
 		// Dispose of any resources that can be recreated.
 	}
 
+	override func viewDidLayoutSubviews() {
+		setZoomParametersForSize(scrollView.frame.size)
+	}
 
 }
 
+extension ViewController: UIScrollViewDelegate {
+	func viewForZooming(in scrollView: UIScrollView) -> UIView? {
+		return imageView
+	}
+}
